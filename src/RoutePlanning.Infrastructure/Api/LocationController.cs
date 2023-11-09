9dcc096d10ff1cb51f9ef64f0c88130216ca00e8
Brunon Lemański <brul@netcompany.com>
2023-11-09 23:44:45 +0100

    location endpoint

---
+++ src/RoutePlanning.Infrastructure/Api/LocationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RoutePlanning.Domain.Locations;
+using RoutePlanning.Infrastructure.Services;
+
+namespace RoutePlanning.Infrastructure.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+[AllowAnonymous]
+public sealed class LocationController : ControllerBase
+{
+    private readonly LocationService _locationService;
+
+    public LocationController(LocationService locationService)
+    {
+        _locationService = locationService;
+    }
+
+    [HttpGet]
+    public Task<List<Location>> GetAllLocations()
+    {
+        return _locationService.GetLocationsAsync();
+    }
+
+}
