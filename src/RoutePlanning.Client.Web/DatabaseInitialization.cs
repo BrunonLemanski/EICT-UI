1b5e2e62abd761ad03b5b3e332da38e7401b2cdb
nc-yia <yia@netcompany.com>
2023-11-09 21:37:05 +0100

    Add orderConfirm

--- src/RoutePlanning.Client.Web/DatabaseInitialization.cs
+++ src/RoutePlanning.Client.Web/DatabaseInitialization.cs
@@ -20,28 +20,101 @@ public static class DatabaseInitialization
             await SeedUsers(context);
             await SeedLocationsAndRoutes(context);
 
-            unitOfWork.Commit();
+            //unitOfWork.Commit();
         }
     }
 
     private static async Task SeedLocationsAndRoutes(RoutePlanningDatabaseContext context)
     {
-        var berlin = new Location("Berlin");
-        await context.AddAsync(berlin);
+        var KANARISKE = new Location("DE KANARISKE ØER");
+        await context.AddAsync(KANARISKE);
 
-        var copenhagen = new Location("Copenhagen");
-        await context.AddAsync(copenhagen);
+        var DAKAR = new Location("DAKAR");
+        await context.AddAsync(DAKAR);
 
-        var paris = new Location("Paris");
-        await context.AddAsync(paris);
+        var SIERRA = new Location("SIERRA LEONE");
+        await context.AddAsync(SIERRA);
 
-        var warsaw = new Location("Warsaw");
-        await context.AddAsync(warsaw);
+        var HELENA = new Location("ST. HELENA");
+        await context.AddAsync(HELENA);
 
-        CreateTwoWayConnection(berlin, warsaw, 573);
-        CreateTwoWayConnection(berlin, copenhagen, 763);
-        CreateTwoWayConnection(berlin, paris, 1054);
-        CreateTwoWayConnection(copenhagen, paris, 1362);
+        var GULD = new Location("GULD KYSTEN");
+        await context.AddAsync(GULD);
+
+        var SLAVE = new Location("SLAVE KYSTEN");
+        await context.AddAsync(SLAVE);
+
+        var HVALBUGTEN = new Location("HVALBUGTEN");
+        await context.AddAsync(HVALBUGTEN);
+
+        var KAPSTADEN = new Location("KAPSTADEN");
+        await context.AddAsync(KAPSTADEN);
+
+        var MARIE = new Location("KAP ST. MARIE");
+        await context.AddAsync(MARIE);
+
+        var MOCAMBIQUE = new Location("MOCAMBIQUE");
+        await context.AddAsync(MOCAMBIQUE);
+
+        var GUARDAFUI = new Location("KAP GUARDAFUI");
+        await context.AddAsync(GUARDAFUI);
+
+        var AMATAVE = new Location("AMATAVE");
+        await context.AddAsync(AMATAVE);
+
+        var SUAKIN = new Location("SUAKIN");
+        await context.AddAsync(SUAKIN);
+
+        var CAIRO = new Location("CAIRO");
+        await context.AddAsync(CAIRO);
+
+        var TUNIS = new Location("TUNIS");
+        await context.AddAsync(TUNIS);
+
+        var TANGER = new Location("TANGER");
+        await context.AddAsync(TANGER);
+
+        CreateTwoWayConnection(KANARISKE, DAKAR, 5);
+        CreateTwoWayConnection(KANARISKE, TANGER, 3);
+        CreateTwoWayConnection(DAKAR, KANARISKE, 5);
+        CreateTwoWayConnection(DAKAR, SIERRA, 3);
+        CreateTwoWayConnection(DAKAR, HELENA, 10);
+        CreateTwoWayConnection(SIERRA, DAKAR, 3);
+        CreateTwoWayConnection(SIERRA, GULD, 4);
+        CreateTwoWayConnection(SIERRA, HELENA, 4);
+        CreateTwoWayConnection(HELENA, SIERRA, 11);
+        CreateTwoWayConnection(HELENA, DAKAR, 10);
+        CreateTwoWayConnection(HELENA, HVALBUGTEN, 10);
+        CreateTwoWayConnection(HELENA, KAPSTADEN, 9);
+        CreateTwoWayConnection(GULD, SIERRA, 4);
+        CreateTwoWayConnection(GULD, SLAVE, 4);
+        CreateTwoWayConnection(GULD, HVALBUGTEN, 11);
+        CreateTwoWayConnection(SLAVE, GULD, 4);
+        CreateTwoWayConnection(SLAVE, HVALBUGTEN, 9);
+        CreateTwoWayConnection(HVALBUGTEN, SLAVE, 9);
+        CreateTwoWayConnection(HVALBUGTEN, GULD, 11);
+        CreateTwoWayConnection(HVALBUGTEN, HELENA, 10);
+        CreateTwoWayConnection(HVALBUGTEN, KAPSTADEN, 3);
+        CreateTwoWayConnection(KAPSTADEN, HELENA, 9);
+        CreateTwoWayConnection(KAPSTADEN, HVALBUGTEN, 3);
+        CreateTwoWayConnection(KAPSTADEN, MARIE, 8);
+        CreateTwoWayConnection(MARIE, KAPSTADEN, 8);
+        CreateTwoWayConnection(MARIE, MOCAMBIQUE, 3);
+        CreateTwoWayConnection(MOCAMBIQUE, MARIE, 3);
+        CreateTwoWayConnection(MOCAMBIQUE, GUARDAFUI, 8);
+        CreateTwoWayConnection(GUARDAFUI, MOCAMBIQUE, 8);
+        CreateTwoWayConnection(GUARDAFUI, AMATAVE, 8);
+        CreateTwoWayConnection(GUARDAFUI, SUAKIN, 4);
+        CreateTwoWayConnection(AMATAVE, GUARDAFUI, 8);
+        CreateTwoWayConnection(SUAKIN, GUARDAFUI, 4);
+        CreateTwoWayConnection(SUAKIN, CAIRO, 4);
+        CreateTwoWayConnection(CAIRO, SUAKIN, 4);
+        CreateTwoWayConnection(CAIRO, TUNIS, 5);
+        CreateTwoWayConnection(TUNIS, CAIRO, 5);
+        CreateTwoWayConnection(TUNIS, TANGER, 3);
+        CreateTwoWayConnection(TANGER, TUNIS, 3);
+        CreateTwoWayConnection(TANGER, KANARISKE, 3);
+
     }
 
     private static async Task SeedUsers(RoutePlanningDatabaseContext context)
