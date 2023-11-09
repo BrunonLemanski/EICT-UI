35d003367fad68f25926fa0674cbcd4391e448a5
Brunon Lemański <brul@netcompany.com>
2023-11-09 21:50:04 +0100

    price entity

---
+++ src/RoutePlanning.Domain/Prices/Price.cs
@@ -0,0 +1,17 @@
+﻿
+using Netcompany.Net.DomainDrivenDesign.Models;
+
+namespace RoutePlanning.Domain.Prices;
+public sealed class Price : Entity<Price>
+{
+    public Price()
+    {
+        Vali
+    }
+
+    public DateTime ValidFrom { get; set; }
+    public DateTime ValidTo { get; set; }
+    public int RangeFrom { get; set; }
+    public int RangeTo { get; set; }
+    public int Cost { get; set; }
+}
