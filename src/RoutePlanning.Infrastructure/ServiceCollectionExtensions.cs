510abeefcdce17ba56f63aa0b3804a7c38a34e79
Brunon Lemański <brul@netcompany.com>
2023-11-09 14:05:39 +0100

    fix errors

--- src/RoutePlanning.Infrastructure/ServiceCollectionExtensions.cs
+++ src/RoutePlanning.Infrastructure/ServiceCollectionExtensions.cs
@@ -4,19 +4,17 @@ using Netcompany.Net.DomainDrivenDesign;
 using Netcompany.Net.UnitOfWork;
 using Netcompany.Net.UnitOfWork.AmbientTransactions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace RoutePlanning.Infrastructure;
 public static class ServiceCollectionExtensions
 
 {
-    public static IServiceCollection AddRoutePlanningInfrastructure(this IServiceCollection services, IConfiguration config )
+    public static IServiceCollection AddRoutePlanningInfrastructure(this IServiceCollection services)
     {
         services.AddDbContext<RoutePlanningDatabaseContext>(builder =>
         {
-            builder.UseSqlServer(config.GetConnectionString("Database"));
+            builder.UseSqlServer("Server=tcp:dbs-eit-dk1.database.windows.net;Database=db-eit-dk1;User Id=admin-eit-dk1;Password=Eastindia4thewin;");
         });
-
 
         services.AddDomainDrivenDesign(options => options.UseDbContext<RoutePlanningDatabaseContext>());
         services.AddUnitOfWork(builder => builder.UseAmbientTransactions().With<RoutePlanningDatabaseContext>());
